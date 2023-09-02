57199113d7698f7ce37534397e606460f5f4694a
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-02 13:13:57 +0300

    Update house_painting.py

--- first_steps/house_painting.py
+++ first_steps/house_painting.py
@@ -2,7 +2,7 @@ x = float(input())
 y = float(input())
 h = float(input())
 
-predna_zadna_steni = (x * x) * 2 - (1.2 * 2)
-stranichni_steni = (x * y) * 2 - 2 * (1.5 * 1.5)
+front_and_back_walls = (x * x) * 2 - (1.2 * 2)
+side_walls = (x * y) * 2 - 2 * (1.5 * 1.5)
 
-pokriv = 2 * (x * y) + 2 * (x * h // 2)
+roof = 2 * (x * y) + 2 * (x * h // 2)
