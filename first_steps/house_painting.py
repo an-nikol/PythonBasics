756c14de1e45cc04fb8f2e67c48241dc9f4b0a09
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-02 13:10:40 +0300

    Create house_painting.py

---
+++ first_steps/house_painting.py
@@ -0,0 +1,8 @@
+x = float(input())
+y = float(input())
+h = float(input())
+
+predna_zadna_steni = (x * x) * 2 - (1.2 * 2)
+stranichni_steni = (x * y) * 2 - 2 * (1.5 * 1.5)
+
+pokriv = 2 * (x * y) + 2 * (x * h // 2)
