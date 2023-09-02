eec5380036909080ead6dcc80ca3a106b9c4d4b0
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-02 13:16:45 +0300

    Delete first_steps/house_painting.py

--- first_steps/house_painting.py
+++
@@ -1,8 +0,0 @@
-x = float(input())
-y = float(input())
-h = float(input())
-
-front_and_back_walls = (x * x) * 2 - (1.2 * 2)
-side_walls = (x * y) * 2 - 2 * (1.5 * 1.5)
-
-roof = 2 * (x * y) + 2 * (x * h // 2)
