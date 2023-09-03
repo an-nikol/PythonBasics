b1cb67251be18305eb789d97a2f109324221d511
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-03 14:35:27 +0300

    Create summer_outfit.py

---
+++ advanced_conditionals/summer_outfit.py
@@ -0,0 +1,36 @@
+temperature = int(input())
+time = input()
+outfit = ''
+shoes = ''
+
+if 10 <= temperature <= 18:
+    if time == 'Morning':
+        outfit = 'Sweatshirt'
+        shoes = 'Sneakers'
+    elif time == 'Afternoon' or time == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+elif 18 <= temperature <= 24:
+    if time == 'Morning':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+    elif time == 'Afternoon':
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+    elif time == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+elif temperature >= 25:
+    if time == 'Morning':
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+    elif time == 'Afternoon':
+        outfit = 'Swim Suit'
+        shoes = 'Barefoot'
+    elif time == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+print(f"It's {temperature} degrees, get your {outfit} and {shoes}.")
