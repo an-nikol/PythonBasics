0fdc5bc49c7b1e78bdf4e4447fc4778f580d3e23
an-nikol <142790207+an-nikol@users.noreply.github.com>
2023-09-03 20:08:17 +0300

    Create 06_task_multiply_table.py

---
+++ exam_15_and_16_july_2023/06_task_multiply_table.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+num_to_str = str(number)
+
+n1 = int(num_to_str[0])
+n2 = int(num_to_str[1])
+n3 = int(num_to_str[2])
+
+for i in range(1, n3 + 1):
+    for j in range(1, n2 + 1):
+        for k in range(1, n1 + 1):
+            summ = i * j * k
+            print(f"{i} * {j} * {k} = {summ};")
